11a49197d0f65c9267d8a15552dda21813320ae5
thanhdat2311 <datmkt2311@gmail.com>
2025-02-21 14:01:34 +0000

    21/2

--- src/app/component/configuration/configuration.component.ts
+++ src/app/component/configuration/configuration.component.ts
@@ -18,10 +18,10 @@ import { trigger, transition, style, animate } from '@angular/animations';
     trigger('slideInOut', [
       transition(':enter', [
         style({ opacity: 0, transform: 'translateY(30px) translateX(100%)' }),
-        animate('500ms ease-out', style({ opacity: 1, transform: 'translateY(0) translateX(0)' }))
+        animate('200ms ease-out', style({ opacity: 1, transform: 'translateY(0) translateX(0)' }))
       ]),
       transition(':leave', [
-        animate('500ms ease-in', style({ opacity: 0, transform: 'translateY(0) translateX(100%)' }))
+        animate('200ms ease-in', style({ opacity: 0, transform: 'translateY(0) translateX(100%)' }))
       ])
     ])
   ]
