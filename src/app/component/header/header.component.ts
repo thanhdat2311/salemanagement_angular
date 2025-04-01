8abc411f9aeb22646eee555bca16b9273daa230c
thanhdat2311 <datmkt2311@gmail.com>
2025-04-01 13:28:56 +0000

    update: dropdown menu user

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserResponse } from 'src/app/response/user/user.response';
 import { TokenService } from 'src/app/services/token.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
   // Variables for drop down menu
   activeAVTDropdown: boolean = false;
-
+  userResponse?: UserResponse|null|undefined;
   constructor(private tokenService:TokenService,
-              private router:Router
+              private router:Router,
+              private userService: UserService
   ){}
+  ngOnInit(): void {
+    this.userResponse = this.userService.getUserFromLocalStorage();
+  }
   // new function
   avtDropdown() {
     if (this.activeAVTDropdown === false) {
